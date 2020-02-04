8ba21f411c82e5d36ae94f9569874da0fb009c9a
Filipe Coelho <filipemcoelho@gmail.com>
2020-02-04 00:14:22 +0000

    Initial Commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Persistence
+{
+  public class DataContext : DbContext
+  {
+    public DbSet<Value> Values { get; set; }
+
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      base.OnModelCreating(modelBuilder);
+
+      modelBuilder.Entity<Value>()
+        .ToTable("values");
+
+      modelBuilder.Entity<Value>().HasData(
+        new Value() { Id = 1, Name = "Value 101" },
+        new Value() { Id = 2, Name = "Value 102" },
+        new Value() { Id = 3, Name = "Value 103" }
+      );
+    }
+
+  }
+}
