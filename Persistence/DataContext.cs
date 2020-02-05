39cbb1fcdac331a2df2d4915b49ee99d17e6af82
Filipe Coelho <filipemcoelho@gmail.com>
2020-02-05 12:39:59 +0000

    End of Section 04

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
-using Domain;
+﻿using Domain;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
-using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace Persistence
 {
   public class DataContext : DbContext
   {
     public DbSet<Value> Values { get; set; }
+    public DbSet<Activity> Activities { get; set; }
 
     public DataContext(DbContextOptions options) : base(options)
     {
@@ -25,12 +19,15 @@ namespace Persistence
       modelBuilder.Entity<Value>()
         .ToTable("values");
 
+      modelBuilder.Entity<Activity>()
+        .ToTable("activities");
+
+
       modelBuilder.Entity<Value>().HasData(
         new Value() { Id = 1, Name = "Value 101" },
         new Value() { Id = 2, Name = "Value 102" },
         new Value() { Id = 3, Name = "Value 103" }
       );
     }
-
   }
 }
---
+++ Persistence/Migrations/20200204134507_InitialState.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class InitialState : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "activities",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Title = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Category = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    City = table.Column<string>(nullable: true),
+                    Venue = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_activities", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "values",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_values", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "values",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 1, "Value 101" });
+
+            migrationBuilder.InsertData(
+                table: "values",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 2, "Value 102" });
+
+            migrationBuilder.InsertData(
+                table: "values",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 3, "Value 103" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "activities");
+
+            migrationBuilder.DropTable(
+                name: "values");
+        }
+    }
+}
