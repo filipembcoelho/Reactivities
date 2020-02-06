5647757daa6047bc4afdf439a6419195d3935ce1
Filipe Coelho <filipemcoelho@gmail.com>
2020-02-06 22:52:09 +0000

    End of Section 10

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,14 +1,37 @@
 import axios, { AxiosResponse } from 'axios';
 import { IActivity } from '../models/activity';
+import { history } from '../..';
+import { toast } from 'react-toastify';
 
 axios.defaults.baseURL = 'http://localhost:5000/api';
+
+axios.interceptors.response.use(undefined, error => {
+  if (error.message === 'Network Error' && !error.response) {
+    toast.error('Network error, did you \'run\' the API');
+  }
+
+  const { status, data, config } = error.response;
+
+  if (status === 404) {
+    history.push('/notfound');
+  }
+
+  if (status === 400 && config.method === 'get' && data.errors.hasOwnProperty('id')) {
+    history.push('/notfound');
+  }
+
+  if (status === 500) {
+    toast.error('Server error - check the terminal for more info!');
+  }
+  console.log(error.response);
+});
 
 const responseBody = (response: AxiosResponse) => response.data;
 
 const sleep = (ms: number) => (response: AxiosResponse) =>
   new Promise<AxiosResponse>(resolve => setTimeout(() => resolve(response), ms));
 
-const time = 2000;
+const time = 1000;
 
 const requests = {
   get: (url: string) => axios.get(url).then(sleep(time)).then(responseBody),
@@ -23,7 +46,7 @@ const Activities = {
   create: (activity: IActivity) => requests.post('/activities', activity),
   update: (activity: IActivity) => requests.put(`/activities/${activity.id}`, activity),
   delete: (id: string) => requests.del(`/activities/${id}`)
-}
+};
 
 export default {
   Activities
